6327dda2a0ee14a6c778170f4ec176e65a418178
VishalVrk <44617366+VishalVrk@users.noreply.github.com>
2025-04-01 19:49:16 +0530

    added firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,12 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDw7i_jwvaUCM2f0oj_g4jYnjgLFOMQv6o",
-    authDomain: "healthcare2025-ec668.firebaseapp.com",
-    projectId: "healthcare2025-ec668",
-    storageBucket: "healthcare2025-ec668.firebasestorage.app",
-    messagingSenderId: "234958159774",
-    appId: "1:234958159774:web:8a47830467632a38ce608f"
+    apiKey: "AIzaSyC4cgroHnb_WrnXAd_iMR8T4OsZNFrvj8w",
+  authDomain: "healthcare-54b85.firebaseapp.com",
+  databaseURL: "https://healthcare-54b85-default-rtdb.firebaseio.com",
+  projectId: "healthcare-54b85",
+  storageBucket: "healthcare-54b85.appspot.com",
+  messagingSenderId: "349262987127",
+  appId: "1:349262987127:web:0c3a95e534d202661aab72",
+  measurementId: "G-3XSRJLGRLK"
 };
 
 const app = initializeApp(firebaseConfig);
